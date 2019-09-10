a3f97e09c733f8be09f2532bce7dc6dcb90e004c
anthowe <ahowe2@att.net>
2019-09-09 20:57:42 -0400

    after back to 4.20

---
+++ Source/S05_TestingGrounds/Terrain/Tile.cpp
@@ -0,0 +1,91 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tile.h"
+#include "Math/UnrealMathUtility.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+
+// Sets default values
+ATile::ATile()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn, float Radius)
+{
+	
+	int NumberToSpawn(FMath::RandRange(MinSpawn, MaxSpawn));
+	for (size_t i = 0; i < NumberToSpawn; i++)
+	{
+		FVector SpawnPoint;
+		bool found = FindEmptyLocation(SpawnPoint, Radius);
+		if (found)
+		{
+			PlaceActor(ToSpawn, SpawnPoint);
+		}
+		
+	}
+
+
+}
+
+bool ATile::FindEmptyLocation(FVector &OutLocation, float Radius)
+{
+	FVector Min(0, -2000, 0);
+	FVector Max(4000, 2000, 0);
+	FBox Bounds(Min, Max);
+	
+
+	const int MAX_ATTEMPTS = 100;
+	for (size_t i = 0; MAX_ATTEMPTS; i++)
+	{
+		FVector CandidatePoint = FMath::RandPointInBox(FBox(Bounds));;
+
+		if (CanSpawnAtLocation(CandidatePoint, Radius))
+		{
+			OutLocation = CandidatePoint;
+			return true;
+		}
+	}
+	return false;
+	
+	
+}
+void ATile::PlaceActor(TSubclassOf<AActor> ToSpawn, FVector SpawnPoint)
+{
+	AActor* Spawned = GetWorld()->SpawnActor<AActor>(ToSpawn);
+	Spawned->SetActorRelativeLocation(SpawnPoint);
+	Spawned->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative, false));
+}
+
+// Called when the game starts or when spawned
+void ATile::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+
+}
+
+// Called every frame
+void ATile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+bool ATile::CanSpawnAtLocation(FVector Location, float Radius)
+{
+	FHitResult HitResult;
+	FVector GlobalLocation = ActorToWorld().TransformPosition(Location);
+	bool HasHit = GetWorld()->SweepSingleByChannel(HitResult, GlobalLocation, GlobalLocation, FQuat::Identity,
+		ECollisionChannel::ECC_GameTraceChannel2, FCollisionShape::MakeSphere(Radius));
+	FColor ColorResult = HasHit ? FColor::Green : FColor::Red;
+	DrawDebugCapsule(GetWorld(), Location, 0, Radius, FQuat::Identity, ColorResult, true, 100);
+	return !HasHit;
+
+
+}
---
+++ Source/S05_TestingGrounds/Terrain/Tile.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Tile.generated.h"
+
+UCLASS()
+class S05_TESTINGGROUNDS_API ATile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ATile();
+
+
+	UFUNCTION(BlueprintCallable, Category = "Spawn")
+		void PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn = 1, int MaxSpawn = 1, float Radius = 500);
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	bool FindEmptyLocation(FVector &FindLocation, float Radius);
+	void PlaceActor(TSubclassOf<AActor> ToSpawn, FVector SpawnPoint);
+	bool CanSpawnAtLocation(FVector Location, float Radius);
+
+};
